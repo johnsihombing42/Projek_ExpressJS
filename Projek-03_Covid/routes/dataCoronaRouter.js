6888cda83e9f87ce65adebe26c4431dd2ba69d9c
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-12 11:34:51 +0700

    update projek3

---
+++ Projek-03_Covid/routes/dataCoronaRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const dataCoronaController = require("../controller/dataCoronaController");
+
+router.get("/", dataCoronaController.index);
+router.get("/create", dataCoronaController.create);
+router.post("/", dataCoronaController.store);
+router.get("/:id/edit", dataCoronaController.edit);
+router.put("/:id", dataCoronaController.update);
+router.delete("/:id", dataCoronaController.destroy);
+
+module.exports = router;
