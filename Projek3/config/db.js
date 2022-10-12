6924efc9b801946f3cc46cf6e2c149b317605446
johnsihombing42 <johnsihombing42@gmail.com>
2022-10-12 11:31:03 +0700

    update management folder

--- Projek3/config/db.js
+++
@@ -1,9 +0,0 @@
-const mysql = require("mysql")
-require("dotenv").config()
-
-module.exports = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-})
